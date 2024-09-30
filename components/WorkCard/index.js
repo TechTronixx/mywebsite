cea7572dca54d6430b8ae8fe5d942f8005ce2a1b
techtronixx <foxstot@gmail.com>
2024-09-30 18:38:29 +0530

    Update Projects card layout to fit images correctly

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -1,21 +1,23 @@
 const WorkCard = ({ img, name, description, onClick }) => {
   return (
     <div
-      className="p-2 overflow-hidden rounded-lg laptop:p-4 first:ml-0 link"
+      className="flex flex-col items-center p-2 overflow-hidden rounded-lg laptop:p-4 first:ml-0 link"
       onClick={onClick}
     >
-      <div className="relative mb-5">
+      <div className="relative mb-5 w-full h-0 pb-[56.25%]">
+        {" "}
+        {/* 16:9 Aspect Ratio */}
         <img
           alt={name}
-          className="object-contain w-full h-full transition-all duration-300 ease-out rounded-t-lg cursor-pointer hover:scale-110"
+          className="absolute top-0 left-0 object-cover w-full h-full transition-all duration-300 ease-out border-2 border-purple-500 rounded-t-lg cursor-pointer hover:scale-110"
           src={img}
         />
       </div>
-      <div>
-        <h1 className="mb-2 text-3xl font-medium">
+      <div className="text-center">
+        <h1 className="py-2 text-3xl font-medium">
           {name ? name : "Project Name"}
         </h1>
-        <h2 className="text-xl opacity-50">
+        <h2 className="py-2 text-xl opacity-50">
           {description ? description : "Description"}
         </h2>
       </div>
